8179f5a426ae20ba7ef3ba489df29dbc5ef99473
Greg Marsh <gregmarsh@Gregs-iMac.local>
2021-02-28 19:25:23 -0800

    fix on models

--- models.py
+++ models.py
@@ -56,10 +56,10 @@ class Review(db.Model):
 class Movie(db.Model):
     __tablename__="movies"
     id=db.Column(db.Integer, primary_key=True)
-    movie_id=db.Column(db.Integer)
+    movie_id=db.Column(db.Integer, unique=True)
     overview=db.Column(db.Text)
     #error
-    title=db.Column(db.Text, unique=True)
+    title=db.Column(db.Text)
     release_date=db.Column(db.DateTime)
     poster_path = db.Column(db.Text)
     vote_average = db.Column(db.Integer)
